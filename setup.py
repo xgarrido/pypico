409da7bfb63b88afb2fafe0312d28c25b07b93f8
Xavier Garrido <xavier.garrido@gmail.com>
2019-02-26 23:33:59 +0100

    comment checklib

--- setup.py
+++ setup.py
@@ -2,23 +2,23 @@ import sys
 from distutils.sysconfig import get_python_inc
 from distutils.version import StrictVersion
 
-#Do some version checking
-skip_version_check=('--skip_version_check' in sys.argv)
-if skip_version_check:
-    sys.argv.remove('--skip_version_check')
-else:
-    skipmsg = "Run with --skip_version_check to suppress this error (PICO may not work correctly)."
-    if (sys.version_info < (2,7)) or (sys.version_info > (3,)):
-        raise Exception("PICO requires Python version 2.7.X. "+skipmsg)
-    def checklib(lib,name,version):
-      try:
-          mod = __import__(lib)
-          if StrictVersion(mod.__version__) < StrictVersion(version):
-              raise Exception("PICO requires %s (>=%s). You have %s. "%(name,version,mod.__version__)+skipmsg)
-      except ImportError:
-          raise Exception("PICO requires %s. "%name+skipmsg)
-    checklib('numpy','NumPy','1.6.1')
-    checklib('scipy','SciPy','0.10.1')
+# #Do some version checking
+# skip_version_check=('--skip_version_check' in sys.argv)
+# if skip_version_check:
+#     sys.argv.remove('--skip_version_check')
+# else:
+#     skipmsg = "Run with --skip_version_check to suppress this error (PICO may not work correctly)."
+#     if (sys.version_info < (2,7)) or (sys.version_info > (3,)):
+#         raise Exception("PICO requires Python version 2.7.X. "+skipmsg)
+#     def checklib(lib,name,version):
+#       try:
+#           mod = __import__(lib)
+#           if StrictVersion(mod.__version__) < StrictVersion(version):
+#               raise Exception("PICO requires %s (>=%s). You have %s. "%(name,version,mod.__version__)+skipmsg)
+#       except ImportError:
+#           raise Exception("PICO requires %s. "%name+skipmsg)
+#     checklib('numpy','NumPy','1.6.1')
+#     checklib('scipy','SciPy','0.10.1')
 
 
 from numpy.distutils.core import setup
@@ -26,7 +26,7 @@ from numpy.distutils.misc_util import Configuration
 
 
 # By default don't try to use Cython to compile the pyx file,
-# just use the file distributed with pypico. 
+# just use the file distributed with pypico.
 build_cython=('--build_cython' in sys.argv)
 if build_cython:
     sys.argv.remove('--build_cython')
